735c5b8be470efdda005d2c19e7d364e15d8dcf3
martinohmann <martinohmann@gmail.com>
2024-01-05 22:37:46 +0100

    chore(authentik): configure some default flows

--- terraform/authentik/flows.tf
+++ terraform/authentik/flows.tf
@@ -1,3 +1,15 @@
 data "authentik_flow" "default-authorization-flow" {
   slug = "default-provider-authorization-implicit-consent"
 }
+
+data "authentik_flow" "default-authentication-flow" {
+  slug = "default-authentication-flow"
+}
+
+data "authentik_flow" "default-invalidation-flow" {
+  slug = "default-invalidation-flow"
+}
+
+data "authentik_flow" "default-user-settings-flow" {
+  slug = "default-user-settings-flow"
+}
--- terraform/authentik/system.tf
+++ terraform/authentik/system.tf
@@ -3,10 +3,13 @@ data "authentik_tenant" "authentik-default" {
 }
 
 resource "authentik_tenant" "home" {
-  domain           = "18b.haus"
-  default          = false
-  branding_title   = "identity.18b.haus"
-  branding_logo    = data.authentik_tenant.authentik-default.branding_logo
-  branding_favicon = data.authentik_tenant.authentik-default.branding_favicon
-  event_retention  = "days=365"
+  domain              = "18b.haus"
+  default             = false
+  branding_title      = "identity.18b.haus"
+  branding_logo       = data.authentik_tenant.authentik-default.branding_logo
+  branding_favicon    = data.authentik_tenant.authentik-default.branding_favicon
+  flow_authentication = data.authentik_flow.default-authentication-flow.id
+  flow_invalidation   = data.authentik_flow.default-invalidation-flow.id
+  flow_user_settings  = data.authentik_flow.default-user-settings-flow.id
+  event_retention     = "days=365"
 }
