abeac8128fe42f9d814bc6d0522528e58261fb72
martinohmann <martinohmann@gmail.com>
2024-03-04 13:42:43 +0100

    fix(terraform): migrate authentik resources to new names

--- terraform/authentik/system.tf
+++ terraform/authentik/system.tf
@@ -1,17 +1,16 @@
-data "authentik_tenant" "authentik-default" {
+data "authentik_brand" "authentik-default" {
   domain = "authentik-default"
 }
 
-resource "authentik_tenant" "home" {
+resource "authentik_brand" "home" {
   domain              = "18b.haus"
   default             = false
   branding_title      = "identity.18b.haus"
-  branding_logo       = data.authentik_tenant.authentik-default.branding_logo
-  branding_favicon    = data.authentik_tenant.authentik-default.branding_favicon
+  branding_logo       = data.authentik_brand.authentik-default.branding_logo
+  branding_favicon    = data.authentik_brand.authentik-default.branding_favicon
   flow_authentication = data.authentik_flow.default-authentication-flow.id
   flow_invalidation   = data.authentik_flow.default-invalidation-flow.id
   flow_user_settings  = data.authentik_flow.default-user-settings-flow.id
-  event_retention     = "days=365"
 }
 
 resource "authentik_service_connection_kubernetes" "local" {
