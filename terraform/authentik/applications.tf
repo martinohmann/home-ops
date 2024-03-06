bef265bf617f6d8f20ddc12d2575d01db3a56134
martinohmann <martinohmann@gmail.com>
2024-03-06 22:25:08 +0100

    fix(kube-web-view): remove OAuth2
    It's internal, it's read-only, it doesn't need auth.

--- terraform/authentik/applications.tf
+++ terraform/authentik/applications.tf
@@ -18,27 +18,6 @@ module "oauth2-grafana" {
   redirect_uris      = ["https://grafana.18b.haus/login/generic_oauth"]
 }
 
-data "kubernetes_secret" "kube-web-view" {
-  metadata {
-    name      = "kube-web-view"
-    namespace = "monitoring"
-  }
-}
-
-module "oauth2-kube-web-view" {
-  source             = "./modules/oauth2-application"
-  name               = "Kube Web View"
-  slug               = "kube-web-view"
-  icon_url           = "https://codeberg.org/repo-avatars/1013-79c19f23d3617c23ec9f668a3c5fe0c5"
-  launch_url         = "https://kube-web-view.18b.haus"
-  newtab             = true
-  auth_groups        = [authentik_group.infra.id, authentik_group.admins.id]
-  authorization_flow = data.authentik_flow.default-authorization-flow.id
-  client_id          = "kube-web-view"
-  client_secret      = data.kubernetes_secret.kube-web-view.data["OAUTH2_CLIENT_SECRET"]
-  redirect_uris      = ["https://kube-web-view.18b.haus/oauth2/callback"]
-}
-
 data "kubernetes_secret" "gitops" {
   metadata {
     name      = "oidc-auth"
