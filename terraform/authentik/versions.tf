68632f46fd08109a2083b8bce67da6949dd8e922
martinohmann <martinohmann@gmail.com>
2024-04-05 22:44:46 +0200

    feat(terraform): add and use kubernetes-secrets module (#583)

--- terraform/authentik/versions.tf
+++ terraform/authentik/versions.tf
@@ -4,9 +4,5 @@ terraform {
       source  = "goauthentik/authentik"
       version = "2024.2.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.27.0"
-    }
   }
 }
---
+++ terraform/kubernetes/secrets/main.tf
@@ -0,0 +1,22 @@
+variable "secrets" {
+  default     = {}
+  description = "Map of arbitrary names to Kubernetes secret references"
+  type = map(object({
+    namespace = string
+    name      = string
+  }))
+}
+
+data "kubernetes_secret" "all" {
+  for_each = var.secrets
+
+  metadata {
+    name      = each.value.name
+    namespace = each.value.namespace
+  }
+}
+
+output "data" {
+  description = "Map of names uses as keys in the `secrets` variable to the referenced secret's data"
+  value       = { for name, secret in data.kubernetes_secret.all : name => secret.data }
+}
