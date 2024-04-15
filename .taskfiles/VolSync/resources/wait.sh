f95255ef6b3939ec91e903c8a87c75c81c441542
martinohmann <martinohmann@gmail.com>
2024-04-15 07:50:28 +0200

    chore: cleanup for multi-cluster support

--- .taskfiles/VolSync/resources/wait.sh
+++ .taskfiles/VolSync/resources/wait.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 JOB=$1
-NAMESPACE="${2:-default}"
+CLUSTER=$2
+NAMESPACE="${3:-default}"
 
 [[ -z "${JOB}" ]] && echo "Job name not specified" && exit 1
 while true; do
-    STATUS="$(kubectl -n "${NAMESPACE}" get pod -l job-name="${JOB}" -o jsonpath='{.items[*].status.phase}')"
+    STATUS="$(kubectl --context "${CLUSTER}" -n "${NAMESPACE}" get pod -l job-name="${JOB}" -o jsonpath='{.items[*].status.phase}')"
     if [ "${STATUS}" == "Pending" ]; then
         break
     fi
