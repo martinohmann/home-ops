6b30fdc6a00fd4222cf07ef897aa94f55ffdf8c9
martinohmann <martinohmann@gmail.com>
2024-07-15 13:36:59 +0200

    feat(kubernetes): install forgejo
    This will replace gitea. I'm going to move the repos over later.

--- terraform/minio/main.tf
+++ terraform/minio/main.tf
@@ -6,6 +6,7 @@ module "main-secrets" {
   secrets = {
     cloudnative-pg  = { namespace = "database", name = "cloudnative-pg" }
     cluster-secrets = { namespace = "flux-system", name = "cluster-secrets" }
+    forgejo         = { namespace = "default", name = "forgejo-secret" }
     gitea           = { namespace = "default", name = "gitea-secret" }
   }
 }
@@ -23,6 +24,7 @@ module "storage-secrets" {
 locals {
   buckets = {
     cloudnative-pg       = module.main-secrets.data.cloudnative-pg["aws-secret-access-key"]
+    forgejo              = module.main-secrets.data.forgejo["minio-secret-access-key"]
     gitea                = module.main-secrets.data.gitea["minio-secret-access-key"]
     terraform-state-sync = module.storage-secrets.data.terraform-state-sync["MINIO_SECRET_KEY"]
     thanos               = module.main-secrets.data.cluster-secrets["SECRET_THANOS_MINIO_SECRET_ACCESS_KEY"]
