ed71ba9679a70572037554ab634af140b653e256
martinohmann <martinohmann@gmail.com>
2024-08-27 20:32:16 +0200

    feat: install netbox (#1301)

--- terraform/minio/main.tf
+++ terraform/minio/main.tf
@@ -7,6 +7,7 @@ module "main-secrets" {
     cloudnative-pg  = { namespace = "database", name = "cloudnative-pg" }
     cluster-secrets = { namespace = "flux-system", name = "cluster-secrets" }
     forgejo         = { namespace = "default", name = "forgejo-secret" }
+    netbox          = { namespace = "default", name = "netbox" }
   }
 }
 
@@ -24,6 +25,7 @@ locals {
   buckets = {
     cloudnative-pg       = module.main-secrets.data.cloudnative-pg["aws-secret-access-key"]
     forgejo              = module.main-secrets.data.forgejo["minio-secret-access-key"]
+    netbox               = module.main-secrets.data.netbox["minio-secret-access-key"]
     terraform-state-sync = module.storage-secrets.data.terraform-state-sync["MINIO_SECRET_KEY"]
     thanos               = module.main-secrets.data.cluster-secrets["SECRET_THANOS_MINIO_SECRET_ACCESS_KEY"]
     volsync              = module.main-secrets.data.cluster-secrets["SECRET_VOLSYNC_MINIO_SECRET_ACCESS_KEY"]
