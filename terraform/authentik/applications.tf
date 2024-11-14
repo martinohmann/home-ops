b7749672d19581e3633e3a1d44cfcaa9e67fbf6b
martinohmann <martinohmann@gmail.com>
2024-11-14 07:39:03 +0100

    refactor(terraform): sort authentik apps by provider + name

--- terraform/authentik/applications.tf
+++ terraform/authentik/applications.tf
@@ -1,3 +1,18 @@
+module "oauth2-forgejo" {
+  source             = "./modules/oauth2-application"
+  name               = "Forgejo"
+  slug               = "forgejo"
+  icon_url           = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/forgejo.svg"
+  launch_url         = "https://forgejo.18b.haus"
+  newtab             = true
+  auth_groups        = [authentik_group.users.id, authentik_group.admins.id]
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
+  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
+  client_id          = "forgejo"
+  client_secret      = module.secrets.data.forgejo["secret"]
+  redirect_uris      = ["https://git.18b.haus/user/oauth2/Authentik/callback"]
+}
+
 module "oauth2-grafana" {
   source             = "./modules/oauth2-application"
   name               = "Grafana"
@@ -10,6 +25,36 @@ module "oauth2-grafana" {
   client_id          = "grafana"
   client_secret      = module.secrets.data.grafana["GF_AUTH_GENERIC_OAUTH_CLIENT_SECRET"]
   redirect_uris      = ["https://grafana.18b.haus/login/generic_oauth"]
+}
+
+module "oauth2-kube-web-view" {
+  source             = "./modules/oauth2-application"
+  name               = "Kube Web View"
+  slug               = "kube-web-view"
+  icon_url           = "https://codeberg.org/repo-avatars/1013-79c19f23d3617c23ec9f668a3c5fe0c5"
+  launch_url         = "https://kube-web-view.18b.haus"
+  newtab             = true
+  auth_groups        = [authentik_group.infra.id, authentik_group.admins.id]
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
+  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
+  client_id          = "kube-web-view"
+  client_secret      = module.secrets.data.kube-web-view["OAUTH2_CLIENT_SECRET"]
+  redirect_uris      = ["https://kube-web-view.18b.haus/oauth2/callback"]
+}
+
+module "oauth2-miniflux" {
+  source             = "./modules/oauth2-application"
+  name               = "Miniflux"
+  slug               = "miniflux"
+  icon_url           = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/miniflux.svg"
+  launch_url         = "https://miniflux.18b.haus"
+  newtab             = true
+  auth_groups        = [authentik_group.users.id, authentik_group.admins.id]
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
+  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
+  client_id          = "miniflux"
+  client_secret      = module.secrets.data.miniflux["OAUTH2_CLIENT_SECRET"]
+  redirect_uris      = ["https://miniflux.18b.haus/oauth2/oidc/callback"]
 }
 
 module "oauth2-minio" {
@@ -42,6 +87,20 @@ module "oauth2-nextcloud" {
   additional_property_mappings = [authentik_property_mapping_provider_scope.openid-nextcloud.id]
 }
 
+module "oauth2-pgadmin" {
+  source             = "./modules/oauth2-application"
+  name               = "pgAdmin"
+  icon_url           = "https://wiki.postgresql.org/images/a/a4/PostgreSQL_logo.3colors.svg"
+  launch_url         = "https://pgadmin.18b.haus"
+  newtab             = true
+  auth_groups        = [authentik_group.infra.id, authentik_group.admins.id]
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
+  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
+  client_id          = "pgadmin"
+  client_secret      = module.secrets.data.pgadmin["OAUTH2_CLIENT_SECRET"]
+  redirect_uris      = ["https://pgadmin.18b.haus/oauth2/authorize"]
+}
+
 module "oauth2-proxmox" {
   source             = "./modules/oauth2-application"
   name               = "Proxmox VE"
@@ -62,70 +121,11 @@ module "oauth2-proxmox" {
   ]
 }
 
-module "oauth2-pgadmin" {
-  source             = "./modules/oauth2-application"
-  name               = "pgAdmin"
-  icon_url           = "https://wiki.postgresql.org/images/a/a4/PostgreSQL_logo.3colors.svg"
-  launch_url         = "https://pgadmin.18b.haus"
-  newtab             = true
-  auth_groups        = [authentik_group.infra.id, authentik_group.admins.id]
-  authorization_flow = data.authentik_flow.default-authorization-flow.id
-  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
-  client_id          = "pgadmin"
-  client_secret      = module.secrets.data.pgadmin["OAUTH2_CLIENT_SECRET"]
-  redirect_uris      = ["https://pgadmin.18b.haus/oauth2/authorize"]
-}
-
-module "oauth2-forgejo" {
-  source             = "./modules/oauth2-application"
-  name               = "Forgejo"
-  slug               = "forgejo"
-  icon_url           = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/forgejo.svg"
-  launch_url         = "https://forgejo.18b.haus"
-  newtab             = true
-  auth_groups        = [authentik_group.users.id, authentik_group.admins.id]
-  authorization_flow = data.authentik_flow.default-authorization-flow.id
-  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
-  client_id          = "forgejo"
-  client_secret      = module.secrets.data.forgejo["secret"]
-  redirect_uris      = ["https://git.18b.haus/user/oauth2/Authentik/callback"]
-}
-
-module "oauth2-miniflux" {
-  source             = "./modules/oauth2-application"
-  name               = "Miniflux"
-  slug               = "miniflux"
-  icon_url           = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/miniflux.svg"
-  launch_url         = "https://miniflux.18b.haus"
-  newtab             = true
-  auth_groups        = [authentik_group.users.id, authentik_group.admins.id]
-  authorization_flow = data.authentik_flow.default-authorization-flow.id
-  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
-  client_id          = "miniflux"
-  client_secret      = module.secrets.data.miniflux["OAUTH2_CLIENT_SECRET"]
-  redirect_uris      = ["https://miniflux.18b.haus/oauth2/oidc/callback"]
-}
-
-module "oauth2-kube-web-view" {
-  source             = "./modules/oauth2-application"
-  name               = "Kube Web View"
-  slug               = "kube-web-view"
-  icon_url           = "https://codeberg.org/repo-avatars/1013-79c19f23d3617c23ec9f668a3c5fe0c5"
-  launch_url         = "https://kube-web-view.18b.haus"
-  newtab             = true
-  auth_groups        = [authentik_group.infra.id, authentik_group.admins.id]
-  authorization_flow = data.authentik_flow.default-authorization-flow.id
-  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
-  client_id          = "kube-web-view"
-  client_secret      = module.secrets.data.kube-web-view["OAUTH2_CLIENT_SECRET"]
-  redirect_uris      = ["https://kube-web-view.18b.haus/oauth2/callback"]
-}
-
-module "proxy-longhorn" {
+module "proxy-filebrowser" {
   source             = "./modules/proxy-application"
-  name               = "Longhorn"
-  icon_url           = "https://raw.githubusercontent.com/longhorn/website/master/static/img/icon-longhorn.svg"
-  slug               = "longhorn"
+  name               = "Filebrowser"
+  icon_url           = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/filebrowser.svg"
+  slug               = "filebrowser"
   domain             = "18b.haus"
   authorization_flow = data.authentik_flow.default-authorization-flow.id
   invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
@@ -137,39 +137,6 @@ module "proxy-home-assistant-code" {
   name               = "Home Assistant Code"
   icon_url           = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/vscode.svg"
   slug               = "home-assistant-code"
-  domain             = "18b.haus"
-  authorization_flow = data.authentik_flow.default-authorization-flow.id
-  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
-  auth_groups        = [authentik_group.admins.id]
-}
-
-module "proxy-redis-commander" {
-  source             = "./modules/proxy-application"
-  name               = "Redis Commander"
-  icon_url           = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/redis.svg"
-  slug               = "redis"
-  domain             = "18b.haus"
-  authorization_flow = data.authentik_flow.default-authorization-flow.id
-  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
-  auth_groups        = [authentik_group.admins.id]
-}
-
-module "proxy-zigbee2mqtt" {
-  source             = "./modules/proxy-application"
-  name               = "Zigbee2MQTT"
-  icon_url           = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/zigbee2mqtt.svg"
-  slug               = "zigbee"
-  domain             = "18b.haus"
-  authorization_flow = data.authentik_flow.default-authorization-flow.id
-  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
-  auth_groups        = [authentik_group.admins.id]
-}
-
-module "proxy-filebrowser" {
-  source             = "./modules/proxy-application"
-  name               = "Filebrowser"
-  icon_url           = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/filebrowser.svg"
-  slug               = "filebrowser"
   domain             = "18b.haus"
   authorization_flow = data.authentik_flow.default-authorization-flow.id
   invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
@@ -202,4 +169,37 @@ module "proxy-kopia-local" {
   basic_auth_enabled            = true
   basic_auth_password_attribute = "kopia_local_password"
   basic_auth_username_attribute = "kopia_local_username"
+}
+
+module "proxy-longhorn" {
+  source             = "./modules/proxy-application"
+  name               = "Longhorn"
+  icon_url           = "https://raw.githubusercontent.com/longhorn/website/master/static/img/icon-longhorn.svg"
+  slug               = "longhorn"
+  domain             = "18b.haus"
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
+  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
+  auth_groups        = [authentik_group.admins.id]
+}
+
+module "proxy-redis-commander" {
+  source             = "./modules/proxy-application"
+  name               = "Redis Commander"
+  icon_url           = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/redis.svg"
+  slug               = "redis"
+  domain             = "18b.haus"
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
+  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
+  auth_groups        = [authentik_group.admins.id]
+}
+
+module "proxy-zigbee2mqtt" {
+  source             = "./modules/proxy-application"
+  name               = "Zigbee2MQTT"
+  icon_url           = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/zigbee2mqtt.svg"
+  slug               = "zigbee"
+  domain             = "18b.haus"
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
+  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
+  auth_groups        = [authentik_group.admins.id]
 }
