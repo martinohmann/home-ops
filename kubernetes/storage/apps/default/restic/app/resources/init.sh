8c31a02c0219ec4a10fa3240dc7247b98ad52a55
martinohmann <martinohmann@gmail.com>
2024-11-18 20:57:53 +0100

    fix(restic): make user management more robust

--- kubernetes/storage/apps/default/restic/app/resources/init.sh
+++ kubernetes/storage/apps/default/restic/app/resources/init.sh
@@ -11,6 +11,15 @@ trap 'rm -f "$temp_file"' EXIT INT
 chmod 0600 "$temp_file"
 
 while IFS=':' read -r username password; do
+  if [ -z "$username" ]; then
+    continue
+  fi
+
+  if [ -z "$password" ]; then
+    echo "Warning: ignoring user $username due to empty password" >&2
+    continue
+  fi
+
   htpasswd -B -b "$temp_file" "$username" "$password"
 done < "$users_file"
 
