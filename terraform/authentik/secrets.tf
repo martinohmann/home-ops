a7efffc64738fb8ff38a1d8f22f2482e53f3e6f8
martinohmann <martinohmann@gmail.com>
2024-12-20 12:29:41 +0100

    feat: reinstall weave-gitops
    The project continues to exist and I actually like it more than
    capacitor.

--- terraform/authentik/secrets.tf
+++ terraform/authentik/secrets.tf
@@ -2,13 +2,15 @@ module "secrets" {
   source = "../kubernetes/secrets"
 
   secrets = {
-    forgejo       = { path = "main/apps/default/forgejo/app/secret.sops.yaml", name = "forgejo-oauth-secret" }
-    grafana       = { path = "main/apps/monitoring/grafana/app/secret.sops.yaml", name = "grafana-secret" }
-    kube-web-view = { path = "main/apps/monitoring/kube-web-view/app/secret.sops.yaml", name = "kube-web-view" }
-    miniflux      = { path = "main/apps/default/miniflux/app/secret.sops.yaml", name = "miniflux" }
-    minio         = { path = "storage/apps/default/minio/app/secret.sops.yaml", name = "minio" }
-    nextcloud     = { path = "main/apps/default/nextcloud/app/secret.sops.yaml", name = "nextcloud-secret" }
-    pgadmin       = { path = "main/apps/database/pgadmin/app/secret.sops.yaml", name = "pgadmin" }
+    forgejo        = { path = "main/apps/default/forgejo/app/secret.sops.yaml", name = "forgejo-oauth-secret" }
+    gitops-main    = { path = "main/apps/flux-system/weave-gitops/app/secret.sops.yaml", name = "oidc-auth" }
+    gitops-storage = { path = "storage/apps/flux-system/weave-gitops/app/secret.sops.yaml", name = "oidc-auth" }
+    grafana        = { path = "main/apps/monitoring/grafana/app/secret.sops.yaml", name = "grafana-secret" }
+    kube-web-view  = { path = "main/apps/monitoring/kube-web-view/app/secret.sops.yaml", name = "kube-web-view" }
+    miniflux       = { path = "main/apps/default/miniflux/app/secret.sops.yaml", name = "miniflux" }
+    minio          = { path = "storage/apps/default/minio/app/secret.sops.yaml", name = "minio" }
+    nextcloud      = { path = "main/apps/default/nextcloud/app/secret.sops.yaml", name = "nextcloud-secret" }
+    pgadmin        = { path = "main/apps/database/pgadmin/app/secret.sops.yaml", name = "pgadmin" }
   }
 }
 
