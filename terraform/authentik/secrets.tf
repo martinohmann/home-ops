d30106e646c9f44ffbe82faee1a1fe7b5fb00fe6
martinohmann <martinohmann@gmail.com>
2025-01-13 22:26:38 +0100

    fix(security): do not expose postgres super password to workload envs (#1804)

--- terraform/authentik/secrets.tf
+++ terraform/authentik/secrets.tf
@@ -5,11 +5,11 @@ module "secrets" {
     forgejo        = { path = "main/apps/default/forgejo/app/secret.sops.yaml", name = "forgejo-oauth-secret" }
     gitops-main    = { path = "main/apps/flux-system/weave-gitops/app/secret.sops.yaml", name = "weave-gitops-oidc-auth" }
     gitops-storage = { path = "storage/apps/flux-system/weave-gitops/app/secret.sops.yaml", name = "weave-gitops-oidc-auth" }
-    grafana        = { path = "main/apps/monitoring/grafana/app/secret.sops.yaml", name = "grafana-secret" }
+    grafana        = { path = "main/apps/monitoring/grafana/app/secret.sops.yaml", name = "grafana" }
     kube-web-view  = { path = "main/apps/monitoring/kube-web-view/app/secret.sops.yaml", name = "kube-web-view" }
     miniflux       = { path = "main/apps/default/miniflux/app/secret.sops.yaml", name = "miniflux" }
     minio          = { path = "storage/apps/default/minio/app/secret.sops.yaml", name = "minio" }
-    nextcloud      = { path = "main/apps/default/nextcloud/app/secret.sops.yaml", name = "nextcloud-secret" }
+    nextcloud      = { path = "main/apps/default/nextcloud/app/secret.sops.yaml", name = "nextcloud" }
     pgadmin        = { path = "main/apps/database/pgadmin/app/secret.sops.yaml", name = "pgadmin" }
     vikunja        = { path = "main/apps/default/vikunja/app/secret.sops.yaml", name = "vikunja-config" }
   }
