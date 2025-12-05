7904ae1decd3d9dd0fae27983261c80479dfb3ef
martinohmann <martinohmann@gmail.com>
2025-12-05 22:28:39 +0100

    chore: archive weave-gitops
    Replaced by headlamp.

--- terraform/authentik/secrets.tf
+++ terraform/authentik/secrets.tf
@@ -2,15 +2,13 @@ module "secrets" {
   source = "../kubernetes/secrets"
 
   secrets = {
-    forgejo        = { path = "main/apps/default/forgejo/app/secret.sops.yaml", name = "forgejo-oauth-secret" }
-    gitops-main    = { path = "main/apps/flux-system/weave-gitops/app/secret.sops.yaml", name = "weave-gitops-oidc-auth" }
-    gitops-storage = { path = "storage/apps/flux-system/weave-gitops/app/secret.sops.yaml", name = "weave-gitops-oidc-auth" }
-    grafana        = { path = "main/apps/monitoring/grafana/app/secret.sops.yaml", name = "grafana" }
-    miniflux       = { path = "main/apps/default/miniflux/app/secret.sops.yaml", name = "miniflux" }
-    minio          = { path = "storage/apps/default/minio/app/secret.sops.yaml", name = "minio" }
-    nextcloud      = { path = "main/apps/default/nextcloud/app/secret.sops.yaml", name = "nextcloud" }
-    pgadmin        = { path = "main/apps/database/pgadmin/app/secret.sops.yaml", name = "pgadmin" }
-    vikunja        = { path = "main/apps/default/vikunja/app/secret.sops.yaml", name = "vikunja-config" }
+    forgejo   = { path = "main/apps/default/forgejo/app/secret.sops.yaml", name = "forgejo-oauth-secret" }
+    grafana   = { path = "main/apps/monitoring/grafana/app/secret.sops.yaml", name = "grafana" }
+    miniflux  = { path = "main/apps/default/miniflux/app/secret.sops.yaml", name = "miniflux" }
+    minio     = { path = "storage/apps/default/minio/app/secret.sops.yaml", name = "minio" }
+    nextcloud = { path = "main/apps/default/nextcloud/app/secret.sops.yaml", name = "nextcloud" }
+    pgadmin   = { path = "main/apps/database/pgadmin/app/secret.sops.yaml", name = "pgadmin" }
+    vikunja   = { path = "main/apps/default/vikunja/app/secret.sops.yaml", name = "vikunja-config" }
   }
 }
 
