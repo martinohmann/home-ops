cbd86708774d44c1a829ed26e27ca7d3c5834877
martinohmann <martinohmann@gmail.com>
2025-12-05 21:35:42 +0100

    feat(headlamp): add storage cluster and switch to proxy auth

--- terraform/authentik/secrets.tf
+++ terraform/authentik/secrets.tf
@@ -6,7 +6,6 @@ module "secrets" {
     gitops-main    = { path = "main/apps/flux-system/weave-gitops/app/secret.sops.yaml", name = "weave-gitops-oidc-auth" }
     gitops-storage = { path = "storage/apps/flux-system/weave-gitops/app/secret.sops.yaml", name = "weave-gitops-oidc-auth" }
     grafana        = { path = "main/apps/monitoring/grafana/app/secret.sops.yaml", name = "grafana" }
-    headlamp       = { path = "main/apps/monitoring/headlamp/app/secret.sops.yaml", name = "headlamp-oidc" }
     kube-web-view  = { path = "main/apps/monitoring/kube-web-view/app/secret.sops.yaml", name = "kube-web-view" }
     miniflux       = { path = "main/apps/default/miniflux/app/secret.sops.yaml", name = "miniflux" }
     minio          = { path = "storage/apps/default/minio/app/secret.sops.yaml", name = "minio" }
